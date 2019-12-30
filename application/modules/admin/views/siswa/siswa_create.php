5b82aa9dd343e13aa9322bd6ffaaa6d8cede0e09
iqbal <iqbalshorfana@gmail.com>
2019-12-30 14:39:59 +0700

    update

--- application/modules/admin/views/siswa/siswa_create.php
+++ application/modules/admin/views/siswa/siswa_create.php
@@ -115,12 +115,6 @@
                   <input type="text" name="status_siswa" class="form-control">
                 </div>
               </div>
-						<div class="form-group row">
-                <label class="col-sm-2 col-form-label">Nama Kelas</label>
-                <div class="col-sm-10">
-                  <input type="text" name="id_kelas" class="form-control">
-                </div>
-              </div>
 </div>
         <div class="col-12">
           <button type="submit" class="btn btn-primary mr-1 mb-1 waves-effect
